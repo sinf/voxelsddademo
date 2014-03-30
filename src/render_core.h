02e7e3880d4b40715aba3c86d47585b36fa175ad
ArhoM <arho.iivari@elisanet.fi>
2014-03-30 22:05:11 +0300

    asdjasnd

--- src/render_core.h
+++ src/render_core.h
@@ -8,7 +8,7 @@
 
 #define MAX_DEPTH_VALUE 100000
 
-#define ENABLE_DAC 1
+#define ENABLE_DAC 0
 
 #define ENABLE_RAYCAST 1
 #define ENABLE_GAMMA_CORRECTION 0 /* enables/disables 3 sqrts per pixel */
