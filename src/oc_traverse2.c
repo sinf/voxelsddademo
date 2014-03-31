c4b4ebff09199397fabf8651bd556c7536ee3cff
ArhoM <arho.iivari@elisanet.fi>
2014-03-31 12:12:41 +0300

    Reduced memory foot print and copying a lot in oc_traverse2.c:oc_traverse_dac() -> huge performance boost

--- src/oc_traverse2.c
+++ src/oc_traverse2.c
@@ -11,86 +11,65 @@
 extern int oc_show_travel_depth;
 extern int oc_detail_level;
 
-#define BUF_RAY_SIZE (6*sizeof(float)+sizeof(uint32))
-typedef struct {
-	size_t len;
-	float *tmin[3], *tmax[3];
-	uint32 *ray_id;
-	float *buf_end;
-} RayBuffer;
+#define fmin(x,y) ((x)<(y)?(x):(y))
+#define fmax(x,y) ((x)>(y)?(x):(y))
 
-static RayBuffer make_buffer( float *p, size_t max_len )
+static size_t intersect_with_aabb( uint32 *ray_ids, size_t num_rays,
+	float const *ray_o[3], float const *ray_d[3],
+	float const aabb_min[3], float const aabb_max[3], float out_depth[] )
 {
-	RayBuffer buf;
-	int x;
-	for( x=0; x<3; x++ ) {
-		buf.tmin[x] = p; p += max_len;
-		buf.tmax[x] = p; p += max_len;
-	}
-	buf.ray_id = (uint32*) p;
-	buf.buf_end = p + max_len;
-	buf.len = 0;
-	return buf;
-}
-
-static RayBuffer filter_rays( RayBuffer const in[], int child_id, uint8 const dead_bits[] )
-{
-	RayBuffer o;
-	size_t r, n;
-	
-	o = make_buffer( in->buf_end, in->len );
-	n = 0;
-	
-	for( r=0; r<in->len; r++ )
+	size_t r;
+	for( r=0; r<num_rays; r++ )
 	{
-		unsigned g;
-		float near, far;
-		size_t id = in->ray_id[r];
+		int k;
+		uint32 id = ray_ids[r];
+		float tmaxs[3], tmins[3];
+		float tmax, tmin;
 		
-		if ( dead_bits[id] )
-			continue;
-		
-		o.ray_id[n] = id;
-		
-		/* Subdivide the interval */
-		for( g=0; g<3; g++ )
+		if ( id + 1 )
 		{
-			float tmin, tmax, split;
-			
-			split = ( in->tmin[g][r] + in->tmax[g][r] ) * 0.5f;
-			
-			if ( child_id & ( 4 >> g ) ) {
-				tmin = split;
-				tmax = in->tmax[g][r];
-			} else {
-				tmin = in->tmin[g][r];
-				tmax = split;
+			for( k=0; k<3; k++ )
+			{
+				float o, d;
+				float t0, t1;
+				
+				d = ray_d[k][id];
+				o = ray_o[k][id];
+				
+				/* d = 1.0f / d; */
+				t0 = ( aabb_min[k] - o ) * d;
+				t1 = ( aabb_max[k] - o ) * d;
+				
+				tmin = fmin( t0, t1 );
+				tmax = fmax( t0, t1 );
+				
+				tmins[k] = tmin;
+				tmaxs[k] = tmax;
 			}
 			
-			o.tmin[g][n] = tmin;
-			o.tmax[g][n] = tmax;
+			tmin = fmax( tmins[0], fmax( tmins[1], tmins[2] ) );
+			tmax = fmin( tmaxs[0], fmin( tmaxs[1], tmaxs[2] ) );
+			
+			out_depth[id] = tmin;
+		}
+		else
+		{
+			tmax = -1;
 		}
 		
-		/* Test for AABB intersection */
-		near = fmax( fmax( o.tmin[0][n], o.tmin[1][n] ), o.tmin[2][n] );
-		far = fmin( fmin( o.tmax[0][n], o.tmax[1][n] ), o.tmax[2][n] );
-		if ( near > far || far < 0.0f ) {
-			continue;
+		if ( tmax < 0.0f || tmin > tmax ) {
+			uint32 *last = --num_rays + ray_ids;
+			ray_ids[r--] = *last;
+			*last = id;
 		}
-		
-		n++;
 	}
-	
-	o.len = n;
-	return o;
+	return num_rays;
 }
 
 static void process_leaf( const OctreeNode *node,
 	int octree_level,
-	RayBuffer *rays,
-	uint8 out_mat[],
-	float out_depth[],
-	uint8 dead_bits[] )
+	uint32 *id_array, size_t num_rays,
+	uint8 out_mat[] )
 {
 	uint8 mat = node->mat;
 	size_t r;
@@ -106,30 +85,24 @@ static void process_leaf( const OctreeNode *node,
 	}
 	#endif
 	
-	for( r=0; r<rays->len; r++ )
+	for( r=0; r<num_rays; r++ )
 	{
-		size_t id;
-		float a, b, c, z;
-		
-		id = rays->ray_id[r];
-		a = rays->tmin[0][r];
-		b = rays->tmin[1][r];
-		c = rays->tmin[2][r];
-		z = fmax( a, fmax( b, c ) );
-		
+		size_t id = id_array[r];
 		out_mat[id] = mat;
-		out_depth[id] = z;
-		dead_bits[id] = 1; /* kill this ray */
+		id_array[r] = 0xFFFFFFFF; /* kill this ray */
 	}
 }
 
 static void traverse_branch( const OctreeNode *node,
 	int octree_level,
-	RayBuffer const *rays,
+	uint32 *id_array, size_t num_rays,
+	float const *ray_o[3], float const *ray_d[3],
 	uint8 out_mat[],
 	float out_depth[],
-	int iter,
-	uint8 dead_bits[] )
+	const int iter,
+	float const aabb_min[3],
+	float const aabb_max[3]
+	)
 {
 	int m;
 	
@@ -139,15 +112,30 @@ static void traverse_branch( const OctreeNode *node,
 	{
 		int child_id = m ^ iter;
 		OctreeNode *child = node->children + child_id;
-		RayBuffer subset = filter_rays( rays, m, dead_bits ); /* why pass m instead of child_id??? */
-		if ( !subset.len ) {
-			/* none of the rays intersected this child node */
-			continue;
+		float child_min[3], child_max[3];
+		size_t subset_len;
+		int k;
+		
+		for( k=0; k<3; k++ ) {
+			float split = ( aabb_min[k] + aabb_max[k] ) * 0.5f;
+			if ( child_id & ( 4 >> k ) ) {
+				child_min[k] = split;
+				child_max[k] = aabb_max[k];
+			} else {
+				child_min[k] = aabb_min[k];
+				child_max[k] = split;
+			}
 		}
+		
+		subset_len = intersect_with_aabb( id_array, num_rays, ray_o, ray_d, child_min, child_max, out_depth );
+		
+		if ( !subset_len )
+			continue;
+		
 		if ( !octree_level || !child->children )
-			process_leaf( child, octree_level, &subset, out_mat, out_depth, dead_bits );
+			process_leaf( child, octree_level, id_array, subset_len, out_mat );
 		else
-			traverse_branch( child, octree_level, &subset, out_mat, out_depth, iter, dead_bits );
+			traverse_branch( child, octree_level, id_array, subset_len, ray_o, ray_d, out_mat, out_depth, iter, child_min, child_max );
 	}
 }
 
@@ -158,30 +146,23 @@ void oc_traverse_dac( const Octree oc[1],
 	uint8 out_mat[],
 	float out_depth[] )
 {
-	uint8 *dead_bits;
-	RayBuffer buffers[8];
-	float *buf_mem;
-	
-	size_t r;
-	float aabb_min = 0;
-	float aabb_max = oc->size;
+	uint32 *id_arrays[8];
+	size_t id_count[8] = {0};
+	uint32 r;
+	float aabb_min[3] = {0};
+	float aabb_max[3];
 	int t;
 	
-	dead_bits = calloc( ray_count, 1 );
-	buf_mem = aligned_alloc( 16, 8 * BUF_RAY_SIZE * ray_count * OCTREE_DEPTH_HARDLIMIT );
+	aabb_max[0] = aabb_max[1] = aabb_max[2] = oc->size;
+	id_arrays[0] = aligned_alloc( 16, 8 * sizeof( uint32 ) * ray_count );
 	
-	buffers[0] = make_buffer( buf_mem, ray_count );
 	for( t=1; t<8; t++ )
-		buffers[t] = make_buffer( buffers[t-1].buf_end, ray_count );
-	for( t=0; t<7; t++ )
-		buffers[t].buf_end = buffers[7].buf_end;
+		id_arrays[t] = id_arrays[0] + t * ray_count;
 	
 	for( r=0; r<ray_count; r++ )
 	{
-		size_t o;
 		int k;
-		int iter = 0;
-		float near, far;
+		uint32 iter = 0;
 		
 		out_mat[r] = 0;
 		out_depth[r] = MAX_DEPTH_VALUE;
@@ -191,44 +172,27 @@ void oc_traverse_dac( const Octree oc[1],
 				/* Ray direction is negative. Child nodes should be traversed in reverse order along this axis. */
 				iter |= 4 >> k;
 			}
+			
+			((float*) ray_d[k])[r] = 1.0f / ray_d[k][r];
 		}
 		
-		o = buffers[iter].len;
-		
-		for( k=0; k<3; k++ ) {
-			float t0, t1, tmin, tmax;
-			float d;
-			
-			/* Intersect ray with the bounding box of the root node */
-			d = ray_d[k][r];
-			t0 = ( aabb_min - ray_o[k][r] ) / d;
-			t1 = ( aabb_max - ray_o[k][r] ) / d;
-			
-			tmin = fmin( t0, t1 );
-			tmax = fmax( t0, t1 );
-			
-			buffers[iter].tmin[k][o] = tmin;
-			buffers[iter].tmax[k][o] = tmax;
-		}
-		
-		near = fmax( fmax( buffers[iter].tmin[0][o], buffers[iter].tmin[1][o] ), buffers[iter].tmin[2][o] );
-		far = fmin( fmin( buffers[iter].tmax[0][o], buffers[iter].tmax[1][o] ), buffers[iter].tmax[2][o] );
-		
-		if ( near < far && far > 0.0f )
-		{
-			buffers[iter].len += 1;
-			buffers[iter].ray_id[o] = r;
-		}
+		id_arrays[iter][ id_count[iter]++ ] = r;
 	}
 	
 	for( t=0; t<8; t++ ) {
 		traverse_branch( &oc->root,
 			oc->root_level - oc_detail_level + 1,
-			&buffers[t],
+			id_arrays[t], id_count[t],
+			ray_o, ray_d,
 			out_mat, out_depth,
-			t, dead_bits );
+			t,
+			aabb_min, aabb_max );
 	}
 	
-	free( buf_mem );
-	free( dead_bits );
+	for( r=0; r<ray_count; r++ )
+	{
+		int k;
+		for( k=0; k<3; k++ )
+			( (float*) ray_d[k] )[r] = 1.0f / ray_d[k][r];
+	}
 }
