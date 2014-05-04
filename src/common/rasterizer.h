cd88698b3ca635993fdb008d83e07e08209c56fe
ArhoM <none>
2014-05-05 00:04:18 +0300

    Managed to get the matrix math right. 8 cube vertices are projected to screen and rasterized. Renamed global camera to the_camera

--- src/common/rasterizer.h
+++ src/common/rasterizer.h
@@ -12,6 +12,10 @@
 
 int draw_box( uint32 c, int x, int y, int w, int h );
 
+/* Returns a mask where each bit tells, whether the corresponding vertex on a cube,
+extending from (n,n,n) to (p,p,p), can be seen by an observer located at (x,y,z) */
+int get_visible_cube_vertices( int x, int y, int z, int n, int p );
+
 /* Verts contains (x,y) pairs. They need to be in counter-clockwise order */
 int draw_polygon( uint32 c, int num_verts, float verts[] );
 
